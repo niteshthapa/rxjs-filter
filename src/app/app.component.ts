3fd3834de4a6423f95a7333b2a3fc9aca85093a6
Nitesh <nitesh_thapa@ymail.com>
2021-08-21 11:26:10 +0530

    rxjs filter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
-
+import { Component,OnInit } from '@angular/core';
+import { from} from 'rxjs';
+import {filter,  toArray} from 'rxjs/operators'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'rxjsfilter';
+export class AppComponent implements OnInit {
+  datas;
+  arr=[
+    {fname:"Nitesh",lname:"Thapa",age:33,city:"Delhi"},
+    {fname:"saro",lname:"aski",age:23,city:"Chandigrah"},
+    {fname:"James",lname:"sohapa",age:33,city:"Kolkata"},
+    {fname:"sadro",lname:"daski",age:63,city:"Chandigrah"},
+  ]
+  ngOnInit() {
+ 
+ from(this.arr).pipe(
+  filter(a=> a.city =="Chandigrah"),
+   toArray()
+ ).subscribe(result=> this.datas = result)
+  }
+
 }
